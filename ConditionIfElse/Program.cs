f9d4d76bc5d15427d566f9981c26164217529438
43bp2 <43bp@mail.ru>
2022-09-28 21:07:47 +0300

    add ConditionIfElse

---
+++ ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "саша") // если username = Саша. ToLower() - переводит всё в нижний регситр во избежание ошибок в написании имени в части регистра букв!!! тут знак ; НЕ СТАВИМ! так как мы еще не закончили!
+{
+Console.WriteLine("Ура, это же Саша!");
+}
+else // иначе
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
