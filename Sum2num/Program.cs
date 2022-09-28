3540cf2f52c382d3ac241754269fc4e2daeb9668
43bp2 <43bp@mail.ru>
2022-09-28 20:51:01 +0300

    add random comm

--- Sum2num/Program.cs
+++ Sum2num/Program.cs
@@ -1,6 +1,6 @@
 ﻿int numberA = new Random().Next(1,10); // 0,1,2,3.....9
-Console.WriteLine(numberA);
+Console.WriteLine(numberA); // выводим загаданное numberA
 int numberB = new Random().Next(1,10); // 0,1,2,3.....9
-Console.WriteLine(numberB);
+Console.WriteLine(numberB); // выводим загаданное numberB
 int sum = numberA + numberB;
 Console.WriteLine(sum);
