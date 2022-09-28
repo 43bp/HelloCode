87aa953b25f9318dff926fedce2dfd21ab7756ef
43bp2 <43bp@mail.ru>
2022-09-28 20:49:38 +0300

    add randome

--- Sum2num/Program.cs
+++ Sum2num/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 32;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); // 0,1,2,3.....9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); // 0,1,2,3.....9
+Console.WriteLine(numberB);
 int sum = numberA + numberB;
 Console.WriteLine(sum);
